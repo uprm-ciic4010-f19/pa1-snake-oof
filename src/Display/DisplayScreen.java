5bc2b97c6bed73b98621ff936c9903294ed29690
rolandor170 <rolandor170@LAPTOP-S24GDDJF.lan>
2019-09-08 01:21:35 -0400

    Changed background color to purple.

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -47,8 +47,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
-
+        canvas.setBackground(new Color(102,0,153));
         frame.add(canvas);
         frame.pack();
     }
