721c9471b132254a8718701b1595c268a91a1f4a
rolandor17 <rolando.rios1@upr.edu>
2019-09-13 18:26:36 -0400

    Added the score image.

--- src/Worlds/WorldOne.java
+++ src/Worlds/WorldOne.java
@@ -16,7 +16,7 @@ public class WorldOne extends WorldBase{
 
         //has to be a number bigger than 20 and even
         GridWidthHeightPixelCount = 60;
-        GridPixelsize = (900/GridWidthHeightPixelCount);
+        GridPixelsize = (780/GridWidthHeightPixelCount);
         playerLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
         appleLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
 
